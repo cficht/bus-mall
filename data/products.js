fa07c4188bf0d71bc2eef6b732ebb27d2259c095
cficht <chris.ficht@gmail.com>
2020-01-27 16:55:14 -0800

    made product.js

---
+++ data/products.js
@@ -0,0 +1,121 @@
+const bag = {
+    name: 'bag',
+    image: '../assets/bag.jpg',
+    id: 'bag'
+};
+
+const banana = {
+    name: 'banana',
+    image: '../assets/banana.jpg',
+    id: 'banana'
+};
+
+const bathroom = {
+    name: 'bathroom',
+    image: '../assets/bathroom.jpg',
+    id: 'bathroom'
+};
+
+const boots = {
+    name: 'boots',
+    image: '../assets/boots.jpg',
+    id: 'boots'
+};
+
+const breakfast = {
+    name: 'breakfast',
+    image: '../assets/breakfast.jpg',
+    id: 'breakfast'
+};
+
+const bubblegum = {
+    name: 'bubblegum',
+    image: '../assets/bubblegum.jpg',
+    id: 'bubblegum'
+};
+
+const chair = {
+    name: 'chair',
+    image: '../assets/chair.jpg',
+    id: 'chair'
+};
+
+const cthulhu = {
+    name: 'cthulhu',
+    image: '../assets/cthulhu.jpg',
+    id: 'cthulhu'
+};
+
+const dogduck = {
+    name: 'dogduck',
+    image: '../assets/dogduck.jpg',
+    id: 'dogduck'
+};
+
+const dragon = {
+    name: 'dragon',
+    image: '../assets/dragon.jpg',
+    id: 'dragon'
+};
+
+const pen = {
+    name: 'pen',
+    image: '../assets/pen.jpg',
+    id: 'pen'
+};
+
+const petsweep = {
+    name: 'petsweep',
+    image: '../assets/petsweep.jpg',
+    id: 'petsweep'
+};
+
+const scissors = {
+    name: 'scissors',
+    image: '../assets/scissors.jpg',
+    id: 'scissors'
+};
+
+const shark = {
+    name: 'shark',
+    image: '../assets/shark.jpg',
+    id: 'shark'
+};
+
+const sweep = {
+    name: 'sweep',
+    image: '../assets/sweep.png',
+    id: 'sweep'
+};
+
+const tauntaun = {
+    name: 'tauntaun',
+    image: '../assets/tauntaun.jpg',
+    id: 'tauntaun'
+};
+
+const unicorn = {
+    name: 'unicorn',
+    image: '../assets/unicorn.jpg',
+    id: 'unicorn'
+};
+
+const usb = {
+    name: 'usb',
+    image: '../assets/usb.gif',
+    id: 'usb'
+};
+
+const watercan = {
+    name: 'watercan',
+    image: '../assets/watercan.jpg',
+    id: 'watercan'
+};
+
+const wineglass = {
+    name: 'wineglass',
+    image: '../assets/wineglass.jpg',
+    id: 'wineglass'
+};
+
+export const products = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogduck, dragon, pen, petsweep, scissors, shark, sweep, tauntaun, unicorn, usb, watercan, wineglass];
